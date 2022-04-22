ce2faf24047246b51e993cedce8457fa41c403f3
kavindu25 <kavindudealwis@gmail.com>
2022-04-22 10:22:32 +0530

    basic app completed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'kavindu',
-#         'USER': 'kavindu',
-#         'PASSWORD': 'pwd',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kavindu',
+        'USER': 'kavindu',
+        'PASSWORD': 'testkavindu',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+        
+#     }
+# }
 
 
 # Password validation
